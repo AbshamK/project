2c3c013a7f03aede8f6471769362fbb0853750eb
absham k <abshuabsham@gmail.com>
2025-02-02 02:10:42 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,9 +3,26 @@ const express = require("express"); // import express
 const app = express(); // calling the express function. 
 
 // handling the requests
-app.use("/", (req, res) => {
-    res.send("hello, from the server!.."); // sending respond.
-})
+
+app.post("/hai", (req, res) => {
+    res.send("data saved successfully...");
+});
+
+app.get("/hai", (req, res) => {
+    res.send({ firstName : "absham", lastName : "abshu"});
+});
+
+app.put("/hai", (req, res) => {
+    res.send("data changed successfully");
+});
+
+app.delete("/hai", (req, res) => {
+    res.send("all the data deleted successfully...");
+});
+
+app.use("/hai", (req, res) => {
+    res.send("HAHAHAHAHAHAHAHAAHAHA.....");
+});
 
 app.listen(3000, ()=> {
     console.log("server started, listening on port no 3000...");
